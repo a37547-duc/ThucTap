0ee6c573033241869cef07498a9b19224f238670
a37547-duc <75719437+a37547-duc@users.noreply.github.com>
2024-12-05 20:36:14 +0700

    Add files via upload

--- config/socket.js
+++ config/socket.js
@@ -7,7 +7,8 @@ const initializeSocket = (httpServer) => {
   // Tạo Socket.IO server gắn vào HTTP server
   io = new Server(httpServer, {
     cors: {
-      origin: "http://localhost:5050",
+      // origin: "http://localhost:5050",
+      origin: "https://quizzlet-19y7.onrender.com",
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
       credentials: true,
     },
